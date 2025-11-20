4dede3e3917c31b23578d2d6a5c63cd6818098a8
anibal1702 <144070680+anibal1702@users.noreply.github.com>
2025-11-20 03:21:43 -0500

    Update navegacion.py

--- modules/navegacion.py
+++ modules/navegacion.py
@@ -23,7 +23,6 @@ def mostrar_menu_lateral():
         # CLIENTE
         if rol_seguro == "CLIENTE":
             st.page_link("pages/Catalogo.py", label="Abrir tienda virtual", icon="🛒")
-            st.divider()
         
         # ADMIN (Usamos 'in' por si dice 'Super Admin' o algo así)
         elif "ADMIN" in rol_seguro:
@@ -38,6 +37,7 @@ def mostrar_menu_lateral():
         
         else:
             st.warning(f"Rol '{rol}' sin accesos configurados.")
+
 
 
 
